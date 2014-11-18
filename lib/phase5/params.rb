f8ab4f259cd2464256e786b57bbf4a43e1e6d4d3
Kyle Smith <akam9865@gmail.com>
2014-11-18 12:38:02 -0800

    phase 5 and 6

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,9 +10,20 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = {}
+      @params.merge!(route_params)
+      
+      if req.query_string
+        @params.merge!(parse_www_encoded_form(req.query_string))
+      end
+      
+      if req.body
+        @params.merge!(parse_www_encoded_form(req.body))
+      end
     end
 
     def [](key)
+      @params[key.to_s]
     end
 
     def to_s
@@ -28,11 +39,38 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+
+      
+      key_values = URI.decode_www_form(www_encoded_form)
+      params = {}
+      
+      # key_values.each do |key, value|
+      #   params[key] = value
+      # end
+      
+      key_values.each do |full_key, value|
+        
+        scope = params
+        
+        parsed_key = parse_key(full_key)
+        
+        parsed_key.each_with_index do |key, index|
+          if index + 1 == parsed_key.length
+            scope[key] = value
+          else
+            scope[key] ||= {}
+            scope = scope[key]
+          end
+        end
+      end
+      
+      params
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
